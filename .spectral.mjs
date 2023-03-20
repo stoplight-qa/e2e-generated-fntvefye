c836f88f3b633e0b2d5de62708ccbc6fb2d68e72
qa-rmdei-0-pzri@example.com <qa-rmdei-0-pzri@example.com>
2023-03-20 19:13:08 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15713.stoplight-dev.com/api/v1/projects/cHJqOjM5OA/spectral.js?branch_slug=main&token=6cf34fc6-b0b7-4430-a61b-13d8fec1bc01";
+export default { extends: ruleset };
